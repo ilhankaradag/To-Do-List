d0d70d8ac1da487f36ce858e0726c834ec437bac
ilhankaradag <ilhankaradag932@gmail.com>
2023-07-05 14:52:31 +0200

    V2

--- main.js
+++ main.js
@@ -8,95 +8,122 @@ class CreateTodo {
   }
 }
 
-// const toDo1 = new CreateTodo('test1');
-// const toDo2 = new CreateTodo('test2');
-// const toDo3 = new CreateTodo('test3');
-// todoItems.push(toDo1);
-// todoItems.push(toDo2);
-// todoItems.push(toDo3);
-// console.log(todoItems);
-
-//Create an event listener for the `"addTask" button` ,with click type event .
 const buttonAddTask = document.getElementById('addTask');
 buttonAddTask.addEventListener('click', addTodo);
 
-//Build the event handler function and call it `addTodo`
 function addTodo() {
-  //Retrieve the task text from the `"inputTask"` input field.
   const inputTask = document.getElementById('inputTask');
   const textInput = inputTask.value;
-  //Check if the task text is empty. If it is, display an alert to the user to enter a task.
-  if (textInput === '') {
-    alert('Please enter a task ');
-  }
-  // If the task text is not empty, create a new todo object using the `createTodo` factory function.
-  if (textInput !== '') {
-    const toDo = new CreateTodo(textInput);
-    console.log(toDo);
-    //Push the todo object into the todoItems array.
-    todoItems.push(toDo);
-    // Logged the array to the console to check if the task object is successfully created or not.
-    console.log(todoItems);
-    renderTodo();
-  }
-  //Call a function called renderTodo
 
-  //Clear the value of the "inputTask" input field
+  if (textInput === '') {
+    alert('Please enter a task');
+    return;
+  }
+
+  const toDo = new CreateTodo(textInput);
+  todoItems.push(toDo);
+  renderTodo();
+
   inputTask.value = '';
-  //Focus
-  const focusButton = document.getElementById('addTask');
-  const focusInput = document.getElementById('inputTask');
-  focusButton.addEventListener('click', () => {
-    focusInput.focus({ focusVisible: true });
-  });
+  inputTask.focus();
 }
 
 function renderTodo() {
   const todoList = document.getElementById('todo-list');
   todoList.innerHTML = '';
+
   todoItems.forEach((toDo) => {
     const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = toDo.checked;
+
     listItem.textContent = toDo.text;
+    listItem.appendChild(checkbox);
+    let span = document.createElement('span');
+    span.innerHTML = '\u00d7';
+    listItem.appendChild(span);
+
     todoList.appendChild(listItem);
+
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        listItem.style.textDecoration = 'line-through';
+        listItem.style.color = 'red';
+        listItem.style.backgroundColor = '#FFEFD5';
+      } else {
+        listItem.style.textDecoration = 'none';
+        listItem.style.color = 'black';
+        listItem.style.backgroundColor = '#FFFACD';
+      }
+    });
+
+    listItem.addEventListener('click', (e) => {
+      if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+      } else if (e.target.tagName === 'SPAN') {
+        const listItem = e.target.parentElement;
+        const checkbox = listItem.querySelector('input[type="checkbox"]');
+        const isChecked = checkbox.checked;
+        const index = Array.from(listItem.parentElement.children).indexOf(
+          listItem,
+        );
+        if (index !== -1) {
+          todoItems.splice(index, 1);
+          renderTodo();
+        }
+      }
+    });
   });
 }
-//Remove
-const removeButton = document.getElementById('removeTask');
-removeButton.addEventListener('click', removeLastTask);
+
+const removeTaskButton = document.getElementById('removeTask');
+removeTaskButton.addEventListener('click', removeLastTask);
+
 function removeLastTask() {
   if (todoItems.length === 0) {
     alert('The list is empty');
+    return;
+  }
+
+  const response = confirm('Are you sure you want to do that?');
+  if (response) {
+    console.log('Ok was pressed');
+    todoItems.shift();
+    renderTodo();
   } else {
-    const response = confirm('Are you sure you want to do that?');
-    if (response) {
-      // add code if the user pressed the Ok button
-      console.log('Ok was pressed');
-      todoItems.pop();
-      renderTodo();
-    } else {
-      // add code if the user pressed the Cancel button
-      console.log('Cancel was pressed');
-      return;
-    }
+    console.log('Cancel was pressed');
   }
 }
-//Remove All
-const removeAllButton = document.getElementById('removeAllTask');
 
+const removeAllButton = document.getElementById('removeAllTaskButton');
 removeAllButton.addEventListener('click', function () {
   if (todoItems.length === 0) {
     alert('The list is empty');
+    return;
+  }
+
+  const response = confirm('Are you sure you want to do that?');
+  if (response) {
+    console.log('Ok was pressed');
+    todoItems = [];
+    renderTodo();
   } else {
-    const response = confirm('Are you sure you want to do that?');
-    if (response) {
-      // add code if the user pressed the Ok button
-      console.log('Ok was pressed');
-      todoItems = [];
-      renderTodo();
-    } else {
-      // add code if the user pressed the Cancel button
-      console.log('Cancel was pressed');
-      return;
-    }
+    console.log('Cancel was pressed');
   }
 });
+
+function getDateTime() {
+  const dateTimeLine = document.getElementById('dateTime');
+  let today = new Date();
+  let date =
+    today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+  let time =
+    today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+  let dateTime = date + '  ' + time;
+  console.log(dateTime);
+  dateTimeLine.innerHTML = dateTime;
+}
+getDateTime();
+setInterval(getDateTime, 1000);
+renderTodo();
